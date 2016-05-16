f1bba1a8b49cc11340cf96ffda57b1e984332a32
Jerome1210 <b99705008@ntu.edu.tw>
2016-05-16 12:24:03 +0800

    test: my first easy test

--- EarlGreyTest/ViewController.swift
+++ EarlGreyTest/ViewController.swift
@@ -12,14 +12,39 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let button = createButton("test1")
+        self.view.addSubview(button)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func createButton(name: String) -> UIButton {
+        let frame = CGRectMake(100, 100, 120, 120)
+        let button = UIButton(type: UIButtonType.System)
+        button.frame = frame
+        button.accessibilityIdentifier = name
+        button.accessibilityLabel = name
+        button.setTitle(name, forState: UIControlState.Normal)
+        button.backgroundColor = UIColor.redColor()
+        button.addTarget(self, action:#selector(ViewController.buttonClicked(_:)), forControlEvents: UIControlEvents.TouchUpInside)
+        return button
+    }
+    
+    func buttonClicked(sender: UIButton) {
+        if let id = sender.accessibilityIdentifier {
+            print("did click \(id).")
+            sender.backgroundColor = getRandomColor()
+        }
+    }
+    
+    func getRandomColor() -> UIColor {
+        let randomRed = CGFloat(drand48())
+        let randomGreen = CGFloat(drand48())
+        let randomBlue = CGFloat(drand48())
+        return UIColor.init(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+    }
 }
 
--- EarlGreyTestTests/EarlGreyTestTests.swift
+++ EarlGreyTestTests/EarlGreyTestTests.swift
@@ -10,27 +10,11 @@ import XCTest
 @testable import EarlGreyTest
 
 class EarlGreyTestTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testBasicSelection() {
+        EarlGrey().selectElementWithMatcher(grey_accessibilityID("test1")).performAction(grey_tap())
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testSufficientlyVisible() {
+        EarlGrey().selectElementWithMatcher(grey_accessibilityID("test1")).assertWithMatcher(grey_sufficientlyVisible())
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+       
 }
