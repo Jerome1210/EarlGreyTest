122a55ec07aa96cdfdaeb22ca8bd9db658fa2a4c
Jerome1210 <b99705008@ntu.edu.tw>
2016-05-16 12:19:30 +0800

    add: files that EarlGrey with swift needed

---
+++ EarlGreyTestTests/EarlGrey.swift
@@ -0,0 +1,13 @@
+//
+//  EarlGrey.swift
+//  EarlGreyTest
+//
+//  Created by Lee on 2016/5/16.
+//  Copyright © 2016年 Lee. All rights reserved.
+//
+
+import Cocoa
+
+class EarlGrey: NSObject {
+
+}
